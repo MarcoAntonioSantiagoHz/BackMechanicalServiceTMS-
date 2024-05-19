da57db0d7b0f31014d51bfb44f63fc2c03e39661
Marco Antonio <86581496+MarcoAntonioSantiagoHz@users.noreply.github.com>
2024-05-19 13:33:50 -0600

    Init Comitt Creacion del proyecto
    Creacion de modelos y instalacion de componentes

---
+++ tokesMagistralService.WebApi/tokesMagistralService.WebApi/Models/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace tokesMagistralService.WebApi.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
