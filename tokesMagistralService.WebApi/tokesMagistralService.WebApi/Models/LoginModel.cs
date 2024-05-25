67ee32fd23fadb4d52b071e310adf681a78a300d
Marco Antonio <86581496+MarcoAntonioSantiagoHz@users.noreply.github.com>
2024-05-24 21:47:13 -0600

    Reload Project

--- tokesMagistralService.WebApi/tokesMagistralService.WebApi/Models/LoginModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace tokesMagistralService.WebApi.Models
-{
-    public class LoginModel
-    {
-        public string Email { get; set; }
-
-        public string Password { get; set; }
-    }
-}
